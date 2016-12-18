5e2d9a8c9fc8cd18e443d91104bffe7034381302
Alexander <alex.c.hayes08@gmail.com>
2016-12-18 05:19:09 -0600

    Added a hash function

--- www/js/common.js
+++ www/js/common.js
@@ -65,3 +65,14 @@ function toggleOffLoadingScreen () {
             console.log("The nav elements are not available on this page");
         });
 })();
+
+function hashCode () {
+  var hash = 0, i, chr, len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
