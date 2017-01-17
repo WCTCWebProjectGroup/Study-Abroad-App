4bbb8bd1128969a65161572c53b31031525fbe25
Alex Hayes <alex.c.hayes08@gmail.com>
2017-01-17 10:40:11 -0600

    Latest

--- www/js/queue.js
+++
@@ -1,9 +0,0 @@
-// Global Variables go in this file
-// All functions queued up cannot accept parameters
-// When linking this file, make sure it's the first script in the header
-function GlobalVars () {
-    var queue = [];
-    function addToQueue() {
-
-    }
-}
